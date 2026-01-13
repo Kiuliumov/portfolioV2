3265725980fcf12ef8cba147636bdad2d76b3976
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2026-01-13 10:55:39 +0200

    Update main.tsx

--- portfoliov2/src/main.tsx
+++ portfoliov2/src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/portfolioV2">
+    <BrowserRouter basename="/portfolioV2/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
