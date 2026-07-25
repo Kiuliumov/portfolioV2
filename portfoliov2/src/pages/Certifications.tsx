2ecc3b022807f223ec22a4de5ace43cacf8a4e6f
Kiuliumov <miti.bania@gmail.com>
2026-07-25 19:16:18 +0300

    Refactor certificates constants

--- portfoliov2/src/pages/Certifications.tsx
+++ portfoliov2/src/pages/Certifications.tsx
@@ -1,56 +1,6 @@
 import React, { useRef } from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
-
-const certificates = [
-  {
-    img: "https://kiuliumov.eu/images/programming-cert.png",
-    link: "https://softuni.bg/certificates/details/197241/e7a86c07",
-  },
-  {
-    img: "https://kiuliumov.eu/images/programming-cert2.png",
-    link: "https://softuni.bg/certificates/details/210696/6673d5d0",
-  },
-  {
-    img: "https://kiuliumov.eu/images/programming-cert3.png",
-    link: "https://softuni.bg/certificates/details/217890/15502660",
-  },
-  {
-    img: "https://kiuliumov.eu/images/programming-cert4.png",
-    link: "https://softuni.bg/certificates/details/222890/ea1d597d",
-  },
-  {
-    img: "https://kiuliumov.eu/images/programming-cert5.png",
-    link: "https://softuni.bg/certificates/details/235907/c68c7389",
-  },
-  {
-    img: "https://kiuliumov.eu/images/orm-certificate.png",
-    link: "https://softuni.bg/certificates/details/240688/f0cccbd8",
-  },
-  {
-    img: "https://kiuliumov.eu/images/django.png",
-    link: "https://softuni.bg/certificates/details/246191/a98a5780",
-  },
-  {
-    img: "https://kiuliumov.eu/images/programming-cert6.jpg",
-    link: "https://softuni.bg/certificates/details/248553/eea6353e",
-  },
-  {
-    img: "https://kiuliumov.eu/images/programming-cert7.jpg",
-    link: "https://softuni.bg/certificates/details/248653/515859e5",
-  },
-  {
-    img: "https://kiuliumov.eu/images/programming-cert8.jpg",
-    link: "https://softuni.bg/certificates/details/248851/15223061",
-  },
-  {
-    img: "https://kiuliumov.eu/images/programming-cert9.png",
-    link: "https://softuni.bg/certificates/details/252812/64e34486",
-  },
-  {
-    img: "https://kiuliumov.eu/images/cert-10.png",
-    link: "https://softuni.bg/certificates/details/257357/7896a680",
-  },
-];
+import { certificates } from "../data/certificates";
 
 const Certifications: React.FC = () => {
   const scrollRef = useRef<HTMLDivElement>(null);
